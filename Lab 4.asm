e132756fe29903f05a795094a2320ff31d20aa07
ryanschlimme <98181475+ryanschlimme@users.noreply.github.com>
2022-03-21 16:32:57 -0500

    Add files via upload
    Bug changing Mx5004 and Mx5005 without any instruction

--- Lab 4.asm
+++ Lab 4.asm
@@ -21,42 +21,56 @@ AND R2, R2, R6;     Isolate bits [15:8] of the first number
 AND R3, R3, R6;     Isolate bits [15:8] of the second number
 AND R7, R7, #0;     Initalize our counter register
 ; Count total entries in array
+LD R0, NUM1;        Load R0/R1 with first two values
+LD R1, NUM2
+LDR R2, R0, #0
+LDR R3, R1, #0
+LD R6, MASK;        Load R6 with mask to start isolating bits [15:8]
+
+AND R2, R2, R6;     Isolate bits [15:8] of the first number
+BRz ENDProgram;     If the first value is the terminator, end the program
+AND R3, R3, R6;     Otherwise, isolate bits [15:8] of the second number
+
+AND R7, R7, #0;     Initalize our counter register
+
 CountLoop;          Count total entries in array
-    LDR R2, R0, #0
-    AND R2, R2, R6
-    BRz CountStop
-    ADD R7, R7, #1
-    ADD R0, R0, #1
-    BRnzp CountLoop
+    LDR R2, R0, #0; Load entry
+    AND R2, R2, R6; Check if entry is terminator
+    BRz CountStop;  If zero, stop counting
+    ADD R7, R7, #1; If not, increment counter
+    ADD R0, R0, #1; Increment pointer
+    BRnzp CountLoop;Start again
 CountStop
+
 LD R0, NUM1;        Reload R0 with x4000
+
 LoopStart 
-    LD R6, MASK
-    LDR R2, R0, #0; Load values
-    AND R2, R2, R6
-    BRz ReturnToStart
-    BRp Continue2
-    LD R5, MASK2
-    AND R2, R5, R2
+    LD R6, MASK;    Load mask
+    LDR R2, R0, #0; Load value 1
+    AND R2, R2, R6; Isolate bits [15:8]
+    BRz ReturnToStart; If zero, return to start
+    BRp Continue2;  If positive continue
+    LD R5, MASK2;   If negative, load mask to remove bit 15
+    AND R2, R5, R2; Remove bit 15
 Continue
-    LDR R3, R1, #0
+    LDR R3, R1, #0; Load value 2
     AND R3, R6, R3
     BRz ReturnToStart
-    BRp Switch
+    BRp Switch;     If first entry negative and second positive, switch numbers, first must be larger unsigned
     AND R3, R5, R3
 Continue2
-    LDR R3, R1, #0
+    LDR R3, R1, #0; Load value 2
     AND R3, R6, R3
     BRz ReturnToStart
-    BRp Continue3
+    BRp Continue3;  If both positive proceed to compare
     AND R3, R5, R3
 Continue3
-    NOT R4, R3
+    NOT R4, R3;     Value 2 - Value 1
     ADD R4, R4, #1
     ADD R5, R4, R2
     BRnz NoAction;  If value is negative or zero, do not swap the numbers
 Switch
-    LDR R2, R0, #0
+    LDR R2, R0, #0; Load initial values
     LDR R3, R1, #0
     STR R2, R1, #0; Store initial values in opposite memory locations
     STR R3, R0, #0
@@ -69,12 +83,12 @@ LD R0, NUM1;        Return R0 to x4000
 LD R1, NUM2;        Return R1 to x4001
 ADD R7, R7, #-1;    Decrement R7
 BRnp LoopStart;     Start Loop again
-LoopStop
 ; End of Bubble Sourt Algorithm
 
 ; Mean Algorithm
 LD R0, NUM1
 LD R2, MEAN
+LD R6, MASK
 LDR R1, R0, #0
 AND R1, R1, R6
 BRz ZERO2
@@ -147,7 +161,7 @@ LD R1, DST
 LD R3, MASK
 Duplicate
 LDR R2, R0, #0
-BRz Subtract25
+BRz NEXT
 AND R2, R2, R3
 STR R2, R1, #0
 ADD R0, R0, #1
@@ -155,80 +169,86 @@ ADD R1, R1, #1
 BRnzp Duplicate
 
 ; Subtract 25 from each value, increment a counter. If positive, ignore. Otherwise, store original value to memory
-AND R7, R7, #0
-LD R6, S
-LD R5, #-25
+NEXT
+AND R4, R4, #0
+AND R5, R5, #0
+AND R6, R6, #0
+AND R3, R3, #0
 
 LD R0, DST
-Subtract25
-LDR R1, R0, #0
-BRz STOPSub25
-ADD R2, R1, R5
-ADD R0, R0, #1
-BRp Subtract25
-ADD R7, R7, #1
-BRnzp Subtract25
-STOPSub25
-STR R7, R6, #0
 
-AND R7, R7, #0
-LD R6, M
-LD R5, #-50
-LD R0, DST
-Subtract50
-LDR R1, R0, #0
-BRz STOPSub50
-ADD R2, R1, R5
+CompareStart
+    LDR R1, R0, #0
+    BRz STOPCompare
+    LD R2, n25
+    ADD R2, R2, R1
+    BRp Compare50
+    ADD R4, R4, #1
+    ADD R0, R0, #1
+    BRnzp CompareStart
+Compare50
+    LD R2, n50
+    ADD R2, R2, R1
+    BRp Compare100
+    ADD R5, R5, #1
+    ADD R0, R0, #1
+    BRnzp CompareStart
+Compare100
+    LD R2, n100
+    ADD R2, R2, R1
+    BRp Greater100
+    ADD R6, R6, #1
+    ADD R0, R0, #1
+    BRnzp CompareStart
+Greater100
+    ADD R3, R3, #1
+    ADD R0, R0, #1
+    BRnzp CompareStart
+STOPCompare
+LD R0, VAL
+LD R1, S
+ADD R4, R1, R4
+STR R4, R0, #0
 ADD R0, R0, #1
-BRp Subtract50
-ADD R7, R7, #1
-BRnzp Subtract50
-STOPSub50
-STR R7, R6, #0
 
-AND R7, R7, #0
-LD R6, L
-LD R5, #-100
-LD R0, DST
-Subtract100
-LDR R1, R0, #0
-BRz STOPSub100
-ADD R2, R1, R5
+LD R1, M
+ADD R5, R1, R5
+STR R5, R0, #0
 ADD R0, R0, #1
-BRp Subtract100
-ADD R7, R7, #1
-BRnzp Subtract100
-STOPSub100
-STR R7, R6, #0
 
-AND R7, R7, #0
-LD R6, H
-LD R5, #-101
-LD R0, DST
-Subtract101
-LDR R1, R0, #0
-BRz STOPSub101
-ADD R2, R1, R5
+LD R1, L
+ADD R6, R1, R6
+STR R6, R0, #0
 ADD R0, R0, #1
-BRp Subtract101
-ADD R7, R7, #1
-BRnzp Subtract101
-STOPSub101
-STR R7, R6, #0
+
+LD R1, H
+ADD R3, R1, R3
+STR R3, R0, #0
+
 
 ENDProgram HALT
 
 NUM1 .FILL x4000;   Memory location of first number
 NUM2 .FILL x4001;   Memory location of first number to compare
-RNG  .FILL x5004;   Memory location of range
-MEAN .FILL x5005;   Memory location of mean
 MASK .FILL x00FF;   Bit mask to isolate bits [7:0]
+
 MASK2 .FILL x7FFF
 MASK3 .FILL xFF00
+
+VAL  .FILL x5000;   
+RNG  .FILL x5004;   Memory location of range
+MEAN .FILL x5005;   Memory location of mean
+
 S    .FILL x5300;   ASCII codes for S, M, L, H 
 M    .FILL x4D00
 L    .FILL x4C00
 H    .FILL x4800
+
 DST  .FILL x3500
+
+n25   .FILL #-25
+n50   .FILL #-50
+n100  .FILL #-100
+n101  .FILL #-101
 
 .END
