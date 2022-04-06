2378b64a60378ac7c21dbb1a2e5580c62620fe6b
ryanschlimme <98181475+ryanschlimme@users.noreply.github.com>
2022-04-06 11:53:06 -0500

    Add files via upload

--- Lab 5.asm
+++ Lab 5.asm
@@ -11,9 +11,9 @@
 .ORIG x3000
 
 ; Prompt Algorithm
-LEA R0, Prompt; Load and display prompt
+LEA R0, Prompt
 PUTS
-LEA R1, CODE;   Load address to store input
+LEA R1, CODE
 LoopStart
 AND R2, R2, #0; Load a register with ASCII of enter key
 ADD R2, R2, x0A
@@ -25,31 +25,32 @@ NOT R0, R0;     Subtract key value from register with enter
 ADD R0, R0, #1
 ADD R2, R0, R2
 BRnp LoopStart; If value is not enter, loop
-ADD R1, R1, #-1;Clear enter from memory
+; Clear enter from memory
+ADD R1, R1, #-1
 STR R2, R1, #0
 ; End of Prompt Algorithm
 
 ; Search/Compare Algorithm
-LD R0, DATA;    Load first pointer
-LDR R1, R0, #0; 
-BRz NoMatch;    If the first pointer is null, there will never be a match
-LDR R0, R1, #0; Otherwise, load the pointer to the first node
-LEA R4, CODE;   Load the input data address
-BRnzp NextNode; Go to the first node
+LD R0, DATA
+LDR R1, R0, #0
+BRz NoMatch
+LDR R0, R1, #0
+LEA R4, CODE
+BRnzp NextNode
 Loop1
-LDR R1, R0, #0; Load the node pointer
-ADD R0, R0, #1; Load the address of the data
+LDR R1, R0, #0
+ADD R0, R0, #1
 LDR R2, R0, #0
-BRz NextNode;   If the address of the data is null, go to the next node
+BRz NextNode
 CompareLoop
-LDR R3, R2, #0; Load the data element
-LDR R5, R4, #0; Load the input element
-BRz Match;      If the input element is zero, there is a match
-NOT R5, R5;     Otherwise subtract
+LDR R3, R2, #0
+LDR R5, R4, #0
+BRz Match
+NOT R5, R5
 ADD R5, R5, #1
 ADD R3, R3, R5
-BRnp NextNode;  If the values are not the same, go to next node
-ADD R4, R4, #1; Otherwise increment data and input addresses
+BRnp NextNode
+ADD R4, R4, #1
 ADD R2, R2, #1
 BRnzp CompareLoop
 Next
@@ -57,26 +58,26 @@ ADD R0, R0, #1
 ADD R4, R4, #1
 BRnzp Loop1
 NextNode
-LEA R4, CODE;   Reload the input data address
-ADD R0, R1, #0; Copy R1 to R0
+LEA R4, CODE
+ADD R0, R1, #0
 ADD R0, R0, #0
-BRnp Loop1; 
-BRz NoMatch;    If there is no data, 
+BRnp Loop1
+BRz NoMatch
 ; End of Search/Compare Algorithm
 
 ; Response Algorithm
 ; If there is a match
 Match 
-LEA R0, CODE;   Load and display the input
+LEA R0, CODE
 PUTS
-LEA R0, YesResponse; Load and display the YesResponse
+LEA R0, YesResponse
 PUTS
-BRnzp EndProgram;    End the program
+BRnzp EndProgram
 ; If there isn't a match
 NoMatch
-LEA R0, CODE;   Load and display the input
+LEA R0, CODE
 PUTS
-LEA R0, NoResponse;  Load and display the NoResponse
+LEA R0, NoResponse
 PUTS
 ; End of Response Algorithm
 
@@ -84,7 +85,7 @@ EndProgram HALT
 
 MASK .FILL xFF00
 DATA .FILL x4000
-Prompt .STRINGZ "Type Course Number and press Enter:"
+Prompt .STRINGZ "Type Course Number and press Enter: "
 YesResponse .STRINGZ " is offered this semester!"
 NoResponse .STRINGZ " is not offered this semester."
 CODE .BLKW #8
